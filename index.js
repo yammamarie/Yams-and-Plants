b8855b726509a456659e1c3ccc50e3557fa22ffb
yammamarie <svalente34@gmail.com>
2020-07-02 11:01:02 -0400

    Copied files
    Copied files from local folder

---
+++ index.js
@@ -0,0 +1,19 @@
+
+
+
+function openProduct(productName,elmnt,color) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+  }
+  document.getElementById(productName).style.display = "block";
+  elmnt.style.backgroundColor = color;
+
+}
+
+document.getElementById("defaultOpen").click();
